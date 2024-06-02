4eed60577f08ac56cfe3ada093b40000ffed6e3d
Stefano Cecchettini <stefano.cecchettini1@gmail.com>
2024-06-02 23:59:12 +0200

    dev

--- job.sh
+++ job.sh
@@ -5,7 +5,7 @@ IMAGE_NAME=matrix_multiplication.sif
 STDOUT_FILE=output.txt
 STDERR_FILE=error.txt
 
-singularity build matrix_multiplication.sif test1.def
+singularity build $IMAGE_NAME test1.def
 singularity instance start $IMAGE_NAME instance1
 singularity run instance://instance1 > $STDOUT_FILE 2> $STDERR_FILE
 singularity instance stop instance1
