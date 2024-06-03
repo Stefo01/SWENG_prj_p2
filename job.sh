c6c24faa164b9b1cfd245144ae2a32922345dda0
Stefo01 <77243277+Stefo01@users.noreply.github.com>
2024-06-03 10:47:47 +0200

    dev

--- job.sh
+++ job.sh
@@ -1,14 +1,3 @@
-#!/bin/bash
-
-IMAGE_NAME=matrix_multiplication.sif
-
-STDOUT_FILE=output.txt
-STDERR_FILE=error.txt
-
-singularity build $IMAGE_NAME test1.def
-singularity instance start $IMAGE_NAME instance1
-singularity run instance://instance1 > $STDOUT_FILE 2> $STDERR_FILE
+singularity instance start matrix_multiplication.sif instance1
+singularity run instance://instance1 > output.txt 2> error.txt
 singularity instance stop instance1
-
-# Rendo eseguibile with chmod +x job.sh
-# scp -> come cp ma tramite ssh    user@... :percorso dove voglio metterlo
