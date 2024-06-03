6e7a25f212b800ed9edbb90b98c417d83e612219
Stefo01 <77243277+Stefo01@users.noreply.github.com>
2024-06-03 12:15:43 +0200

    dev

--- job.sh
+++ job.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+#SBATCH --job-name=run_matrix_mult
+#SBATCH --ntasks=1
+#SBATCH --time=10:00
+
 singularity instance start matrix_multiplication.sif instance1
 singularity run instance://instance1 > output.txt 2> error.txt
 singularity instance stop instance1
